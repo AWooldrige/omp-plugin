338aaf7a8c6a3a2873a0e1777ca4c42990ec8890
Alistair Wooldrige <agw@woolie.co.uk>
2012-01-12 21:28:22 +0000

    Added header details to main plugin php file

---
+++ omp-plugin.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Plugin Name: OnMyPlate.co.uk Recipe Formatter
+Plugin URI: https://github.com/AWooldrige/omp-plugin
+Description: Formatting for recipes on OnMyPlate.co.uk
+Version: 0.1.0
+Author: Alistair Wooldrige
+Author URI: http://onmyplate.co.uk
+License: GPLv2 or later
+*/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+define('OMP_PLUGIN_VERSION', '0.1.0');
+define('OMP_PLUGIN_URL', plugin_dir_url( __FILE__ ));

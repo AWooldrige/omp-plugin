ae2cd67daa23d2e4d72912599d6f5aa40ca5b869
Alistair Wooldrige <agw@woolie.co.uk>
2012-01-14 13:26:29 +0000

    Added rawText property to ComponentParserAbstract, with getters and setters

--- lib/ComponentParserAbstract.php
+++ lib/ComponentParserAbstract.php
@@ -1,6 +1,8 @@
 <?php
 abstract class ComponentParserAbstract {
 
+    private $rawText;
+
     /**
      * Parses the supplied text, returned the post consumed text. Any
      * extracted data shouldn't be returned with this.
@@ -9,4 +11,11 @@ abstract class ComponentParserAbstract {
      * @return string the post consumed text
      */
     abstract protected function parse($text);
+
+    public function setRawText($text) {
+        $this->rawText = $text;
+    }
+    public function getRawText() {
+        return $this->rawText;
+    }
 }

41f3129c07e0046fa23760801d5456b25e5a9b87
Alistair Wooldrige <agw@woolie.co.uk>
2012-01-14 11:53:30 +0000

    Converted tests for splitOnParagraphs which were originally in individual function format. They now use a dataprovider

--- lib-test/ComponentParserAbstractTest.php
+++ lib-test/ComponentParserAbstractTest.php
@@ -12,66 +12,6 @@ class ComponentParserAbstractTest extends PHPUnit_Framework_TestCase {
         $this->stub = $this->getMockForAbstractClass('ComponentParserAbstract');
     }
 
-/*
-    public function test_splitParagraphs_with_none() {
-        $original = "A lovely document with lots of character and class";
-        $expected = array(
-            "A lovely document with lots of character and class"
-        );
-        $this->assertEquals($this->stub->splitParagraphs($original), $expected);
-    }
-    public function test_splitParagraphs_with_simple_paragraph() {
-        $original = "A lovely document with".PHP_EOL.PHP_EOL."lots of character and class";
-        $expected = array(
-            "A lovely document with",
-            "lots of character and class"
-        );
-        $this->assertEquals($this->stub->splitParagraphs($original), $expected);
-    }
-    public function test_splitParagraphs_with_lots_of_linebreaks() {
-        $original = "A lovely document with".PHP_EOL.PHP_EOL.PHP_EOL.PHP_EOL."lots of character and class";
-        $expected = array(
-            "A lovely document with",
-            "lots of character and class"
-        );
-        $this->assertEquals($this->stub->splitParagraphs($original), $expected);
-    }
-    public function test_splitParagraphs_with_whitespace_only_in_blank_line() {
-        $original = "A lovely document with".PHP_EOL.' '.PHP_EOL."lots of character and class";
-        $expected = array(
-            "A lovely document with",
-            "lots of character and class"
-        );
-        $this->assertEquals($this->stub->splitParagraphs($original), $expected);
-    }
-    public function test_splitParagraphs_with_normal_linebreaks_included() {
-        $original = "A lovely".PHP_EOL."document with".PHP_EOL.PHP_EOL."lots of character".PHP_EOL."and class";
-        $expected = array(
-            "A lovely".PHP_EOL."document with",
-            "lots of character".PHP_EOL."and class"
-        );
-        $this->assertEquals($this->stub->splitParagraphs($original), $expected);
-    }
-    public function test_splitParagraphs_with_bad_expected() {
-        $original = "A lovely document with".PHP_EOL.' '.PHP_EOL."lots of character and class";
-        $notexpected = array(
-            "A lovely document with",
-            " ",
-            "lots of character and class"
-        );
-        $this->assertNotEquals($this->stub->splitParagraphs($original), $notexpected);
-    }
-    public function test_splitParagraphs_with_mixed_data() {
-        $original = "A lovely".PHP_EOL.PHP_EOL."=== document === with".PHP_EOL.' '.PHP_EOL."lots of".PHP_EOL.PHP_EOL.PHP_EOL."character".PHP_EOL." - and class";
-        $expected = array(
-            'A lovely',
-            '=== document === with',
-            'lots of',
-            "character".PHP_EOL." - and class",
-        );
-        $this->assertEquals($this->stub->splitParagraphs($original), $expected);
-    }
-*/
 
     /**
      * @dataProvider dataProvider_splitOnParagraphs
@@ -88,6 +28,7 @@ class ComponentParserAbstractTest extends PHPUnit_Framework_TestCase {
 
     public function dataProvider_splitOnParagraphs() {
         return array(
+            //Mixed Data
             array("A lovely".PHP_EOL.PHP_EOL."=== document === with".PHP_EOL.
                   ' '.PHP_EOL."lots of".PHP_EOL.PHP_EOL.PHP_EOL."character".
                   PHP_EOL." - and class",
@@ -95,6 +36,47 @@ class ComponentParserAbstractTest extends PHPUnit_Framework_TestCase {
                         '=== document === with',
                         'lots of',
                         "character".PHP_EOL." - and class"),
+                  true),
+
+            //Test that a test will fail, using a bad expected
+            array("A lovely document with".PHP_EOL.' '.PHP_EOL.
+                  "lots of character and class",
+                  array("A lovely document with",
+                        " ",
+                        "lots of character and class"),
+                 false),
+
+            //Normal linebreaks included
+            array("A lovely".PHP_EOL."document with".PHP_EOL.PHP_EOL.
+                  "lots of character".PHP_EOL."and class",
+                  array("A lovely".PHP_EOL."document with",
+                        "lots of character".PHP_EOL."and class"),
+                  true),
+
+            //With a line only including whitespace
+            array("A lovely document with".PHP_EOL.' '.PHP_EOL.
+                  "lots of character and class",
+                  array("A lovely document with",
+                        "lots of character and class"),
+                  true),
+
+            //Lotes of linebreaks
+            array("A lovely document with".PHP_EOL.PHP_EOL.PHP_EOL.PHP_EOL.
+                  "lots of character and class",
+                  array("A lovely document with",
+                        "lots of character and class"),
+                  true),
+
+            //Simple paragraph
+            array("A lovely document with".PHP_EOL.PHP_EOL.
+                  "lots of character and class",
+                  array("A lovely document with",
+                        "lots of character and class"),
+                  true),
+
+            //No linebreaks at all
+            array("A lovely document with lots of character and class",
+                  array("A lovely document with lots of character and class"),
                   true)
         );
     }

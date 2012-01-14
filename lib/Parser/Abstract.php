e576d1bdbcd302bd7201fb67a8d3841d8fb09f89
Alistair Wooldrige <agw@woolie.co.uk>
2012-01-14 12:52:48 +0000

    Removed the splitOnParagraphs function from the Parser_Abstract

--- lib/Parser/Abstract.php
+++ lib/Parser/Abstract.php
@@ -8,18 +8,6 @@ abstract class Parser_Abstract {
     abstract public function parse();
 
     /**
-     * Splits the recipe into paragraphs. Not bothered how many linebreaks
-     * inbetween paragraphs either. Expects all linebreaks to be represented
-     * by the PHP_EOL delimeter.
-     *
-     * @param string $raw Original recipe to split into paragraphs
-     * @return array array of paragraphs
-     */
-    public function splitParagraphs($raw) {
-        return preg_split('/'.PHP_EOL."(\s|".PHP_EOL.")*".PHP_EOL.'/', $raw);
-    }
-
-    /**
      * Splits line breaks into seperate array entries. If there are no new
      * linebreaks in the string, an array with one element is still returned.
 

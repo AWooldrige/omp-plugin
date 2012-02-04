4d8f6e84658d2870fd1bd3b76d3ea1868a34063f
Alistair Wooldrige <agw@woolie.co.uk>
2012-02-04 10:49:44 +0000

    Implement and test the OMP_Parser_Component_Method parse() method to parse method data from a recipe.

--- lib-test/OMP/Parser/Component/MethodTest.php
+++ lib-test/OMP/Parser/Component/MethodTest.php
@@ -8,5 +8,121 @@ class OMP_Parser_Component_MethodTest extends PHPUnit_Framework_TestCase {
         $this->component = new OMP_Parser_Component_Method();
     }
 
+    public function test_parse_general_case() {
 
+        /**
+         * Test that it works in a general case
+         */
+        $rawText = <<<RECIPE
+More text over
+
+multiple paragraphs
+
+=== Ingredients ===
+Dummy Ingredient
+
+=== Method ===
+- Do this
+- Then this, but this
+    - Subtask 1
+    - Subtask 2
+
+=== Random for Random ===
+Test
+RECIPE;
+
+        $expectedData = array(
+            array(
+                'item' => 'Do this',
+                'subitems' => null
+            ),
+            array(
+                'item' => 'Then this, but this',
+                'subitems' => array(
+                    array(
+                        'item' => 'Subtask 1',
+                        'subitems' => null
+                    ),
+                    array(
+                        'item' => 'Subtask 2',
+                        'subitems'=> null
+                    )
+                )
+            )
+        );
+
+        $this->assertEquals($expectedData, $this->component->parse($rawText));
+    }
+
+    public function test_parse_with_extra_method() {
+
+        /**
+         * Test that it works in a general case
+         */
+        $rawText = <<<RECIPE
+More text over
+
+multiple paragraphs
+
+=== Ingredients ===
+Dummy Ingredient
+
+=== Method ===
+- Do this
+- Then this, but this
+- Subtask 1
+- Subtask 2
+
+=== Method for Test ===
+- Another method item
+    - And another
+
+=== Random for Random ===
+Test
+RECIPE;
+
+        $expectedData = array(
+            array(
+                'item' => 'Do this',
+                'subitems' => null
+            ),
+            array(
+                'item' => 'Then this, but this',
+                'subitems' => array(
+                    array(
+                        'item' => 'Subtask 1',
+                        'subitems' => null
+                    ),
+                    array(
+                        'item' => 'Subtask 2',
+                        'subitems'=> null
+                    )
+                )
+            )
+        );
+
+        $this->setExpectedException('InvalidArgumentException');
+        $this->assertEquals($expectedData, $this->component->parse($rawText));
+    }
+
+    /**
+     * Make sure the world doesn't cave in if no method section is provided
+     */
+    public function test_parse_with_no() {
+        $rawText = <<<RECIPE
+More text over
+
+multiple paragraphs
+
+=== Ingredients ===
+Dummy Ingredient
+
+
+=== Random for Random ===
+Test
+RECIPE;
+
+        $expectedData = null;
+        $this->assertEquals($expectedData, $this->component->parse($rawText));
+    }
 }
--- lib/OMP/Parser/Component/Method.php
+++ lib/OMP/Parser/Component/Method.php
@@ -21,7 +21,38 @@ class OMP_Parser_Component_Method extends OMP_Parser_Component_Abstract {
     public function parse($text) {
         $this->rawText = $text;
 
+        $data = array();
         $paragraphs = OMP_Utilities::splitOnParagraphs($text);
+        $methodFound = false;
 
+        //Can only have one method paragraph
+        foreach($paragraphs as $p) {
+              $lines = OMP_Utilities::splitOnNewlines($p);
+
+              try {
+                  $sectionHeader = $this->parseSectionHeader($lines[0]);
+
+                  if($sectionHeader['type'] != self::SECTION_HEADER) {
+                      throw new InvalidArgumentException('Section Header does not match an Method Header. Looking for: '.self::SECTION_HEADER.' in line: ' . $lines[0]);
+                  }
+              }
+              catch(InvalidArgumentException $e) {
+                  $this->postConsumed[] = $p;
+                  continue;
+              }
+
+            if(!$methodFound) {
+                //Can assume at this point that this paragraph is an attempt at
+                //an ingredients paragraph
+                $list = new OMP_Parser_Generic_List(
+                    OMP_Utilities::mergeOnNewlines(array_slice($lines, 1)), '-');
+                $methodFound = true;
+                $data = $list->parse();
+            }
+            else {
+                throw new InvalidArgumentException('Only one method section is allowed per recipe');
+            }
+        }
+        return (count($data) > 0) ? $data : null;
     }
 }

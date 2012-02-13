056fdb6396bb2d146188d461b595937a286f4add
Alistair Wooldrige <agw@woolie.co.uk>
2012-02-13 16:37:31 +0000

    Add another OMP_Parser_Sequential test, involving 2 Ingredients sections and 1 Method section active

--- lib-test/OMP/Parser/SequentialTest.php
+++ lib-test/OMP/Parser/SequentialTest.php
@@ -7,7 +7,7 @@ class OMP_Parser_SequencialTest extends PHPUnit_Framework_TestCase {
         $this->parser = new OMP_Parser_Sequential();
     }
 
-    public function test_parse() {
+    public function test_parse_with_only_method() {
         $rawText = <<<RECIPE
 This is more text
 
@@ -36,6 +36,75 @@ POSTCONSUMPTION;
             'Method' => array(
                 array(
                     'item' => 'Test method',
+                    'subitems' => null
+                )
+            )
+        );
+
+
+        $data = $this->parser->parse($rawText, $activeComponents);
+        $postConsumed = $this->parser->getPostConsumedText();
+
+        $this->assertEquals($expectedData, $data);
+        $this->assertEquals($expectedPostConsumption, $postConsumed);
+    }
+
+
+    public function test_parse_with_two_ingredient_sections_and_method() {
+        $rawText = <<<RECIPE
+This is more text
+
+=== Ingredients ===
+Test Ingredient - 2 tsp
+
+=== Ingredients for Component ===
+Test Ingredient - 1 whole - sliced
+
+=== Method ===
+ - Test method
+ - Test another method
+
+=== Tips ===
+ - This is a tip
+
+Some random text
+RECIPE;
+
+        $activeComponents = array('Ingredients', 'Method');
+
+        $expectedPostConsumption = <<<POSTCONSUMPTION
+This is more text
+
+=== Tips ===
+ - This is a tip
+
+Some random text
+POSTCONSUMPTION;
+
+        $expectedData = array(
+            'Ingredients' => array(
+                '_' => array(
+                    array(
+                        'name'      => 'Test Ingredient',
+                        'quantity'  => '2 tsp',
+                        'directive' => null
+                    )
+                ),
+                'Component' => array(
+                    array(
+                        'name'      => 'Test Ingredient',
+                        'quantity'  => '1 whole',
+                        'directive' => 'sliced'
+                    )
+                )
+            ),
+            'Method' => array(
+                array(
+                    'item' => 'Test method',
+                    'subitems' => null
+                ),
+                array(
+                    'item' => 'Test another method',
                     'subitems' => null
                 )
             )

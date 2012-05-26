92400444333a64ad617a566763aa26e3a8986969
Alistair Wooldrige <agw@woolie.co.uk>
2012-05-26 10:21:44 +0100

    Removing config.json from bootstrap, adding basic action to hard modify the post variable, as using a filter isn't flexible enough for this plugin's needs

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,4 @@
 <?php
-define('OMP_ENV', 'development');
 
 /**
  * Zend Framework bootstrap loader.
@@ -19,8 +18,3 @@ set_include_path(get_include_path() .
 require_once('Zend/Loader/Autoloader.php');
 $autoloader = Zend_Loader_Autoloader::getInstance();
 $autoloader->registerNamespace('OMP_');
-
-//Setup Zend Registry from JSON config file
-Zend_Registry::set('config',
-                   new Zend_Config_Json('config.json', OMP_ENV));
-
--- omp-plugin.php
+++ omp-plugin.php
@@ -27,3 +27,40 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 define('OMP_PLUGIN_VERSION', '0.1.0');
 define('OMP_PLUGIN_URL', plugin_dir_url( __FILE__ ));
+
+require_once('bootstrap.php');
+
+remove_filter('the_content', 'wpautop');
+remove_filter('the_content', 'wptexturize');
+remove_filter('the_excerpt', 'wpautop');
+remove_filter('the_excerpt', 'wptexturize');
+remove_filter('the_title', 'wpautop');
+remove_filter('the_title', 'wptexturize');
+
+if(!is_admin()) {
+    add_action('wp', 'ompContentFilter');
+}
+
+/**
+ * Parser for the content returned by the database.
+ *
+ * The content returned from the database is parsed using the OMP_Parser,
+ * any leftover content returned. This leftover is usually the post excerpt, and
+ * any text included other than the known formats.
+ *
+ * @access public
+ * @return void
+ */
+function ompContentFilter() {
+    $activeComponents = array('Ingredients', 'Method', 'Tips', 'Text', 'Meta');
+
+    global $posts;
+    foreach($posts as $p) {
+
+        $parser = new OMP_Parser_Sequential();
+        $p->recipe_data = $parser->parse($p->post_content, $activeComponents);
+        $p->post_content = $p->recipe_data['Text']['summary'] .
+               $p->recipe_data['Text']['other'];
+        $p->post_content_filtered = $p->post_content;
+    }
+}

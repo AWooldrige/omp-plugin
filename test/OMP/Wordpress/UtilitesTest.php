68a10bcdd100274009533d3f805453160270b8e4
Alistair Wooldrige <agw@woolie.co.uk>
2013-02-15 18:00:37 +0000

    Add convertHumanDurationToIso8601 utility function

--- test/OMP/Wordpress/UtilitesTest.php
+++ test/OMP/Wordpress/UtilitesTest.php
@@ -11,7 +11,6 @@ class OMP_Wordpress_UtilitiesTest extends PHPUnit_Framework_TestCase {
             OMP_Wordpress_Utilities::oneStickyPost($old, $new)
         );
     }
-
     public function dataProvider_oneStickyPost() {
         return array(
             //Old array contains more than one post (e.g. first time using the
@@ -49,6 +48,29 @@ class OMP_Wordpress_UtilitiesTest extends PHPUnit_Framework_TestCase {
                 array(10,2,4,8),
                 array(8)
             )
+        );
+    }
+
+
+    /**
+     * @dataProvider dataProvider_convertHumanDurationToIso8601
+     */
+    public function test_convertHumanDurationToIso8601($human, $iso) {
+        $this->assertEquals(
+            $iso,
+            OMP_Wordpress_Utilities::convertHumanDurationToIso8601($human)
+        );
+    }
+    public function dataProvider_convertHumanDurationToIso8601() {
+        return array(
+            array('5 days', 'P5D'),
+            array('9 years 5 days', 'P9Y5D'),
+            array('6 days, 80 seconds', 'P6DT80S'),
+            array('2min', 'PT2M'),
+            array('2 min', 'PT2M'),
+            array('2 mins', 'PT2M'),
+            array('4 hours, 20 mins', 'PT4H20M'),
+            array('9years2months5days60hours40minutes1second', 'P9Y2M5DT60H40M1S')
         );
     }
 }
